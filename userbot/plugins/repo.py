45d88623ebdf3b37a38beccad25b1c66726a88cb
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-06-23 16:07:01 +0530

    Update repo.py

--- userbot/plugins/repo.py
+++ userbot/plugins/repo.py
@@ -1,1 +1,16 @@
+import asyncio
+import os
+import requests
+import time
+from PIL import Image
+from io import BytesIO
+from datetime import datetime
+import random
+from telethon import events, version
+from userbot.utils import admin_cmd, sudo_cmd
+from userbot import ALIVE_NAME, Lastupdate
+from . import dcdef
+from telethon.tl.types import ChannelParticipantsAdmins
+# 🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "MAHADEV TANDAV USERBOT"
 
