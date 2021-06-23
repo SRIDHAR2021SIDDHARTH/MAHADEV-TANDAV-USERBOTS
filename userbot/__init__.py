a79b8a2774c592cb429dc8cd5a7cc16a8de592de
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-06-23 15:22:21 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -131,7 +131,7 @@ if bool(ENV):
     # Upstream Repo
     UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/DARK-COBRA/DARKCOBRA.git")
+    "https://github.com/SRIDHAR2021SIDDHARTH/MAHADEV-TANDAV-USERBOT.git")
 
     # Last.fm Module
     BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
