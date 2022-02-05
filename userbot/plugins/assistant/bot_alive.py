7f9f78b60ca62c5092ceefe7c41189db3f79815f
SIDDHARTH SRIDHAR <84910325+SRIDHAR2021SIDDHARTH@users.noreply.github.com>
2022-02-05 21:31:32 +0530

    Update bot_alive.py

--- userbot/plugins/assistant/bot_alive.py
+++ userbot/plugins/assistant/bot_alive.py
@@ -2,7 +2,7 @@ from telethon import events
 
 from userbot import ALIVE_NAME, bot
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "𝕄𝔸ℍ𝔸𝔻𝔼𝕍 𝕌𝕊𝔼ℝ𝔹𝕆𝕋"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "𝕄𝔸ℍ𝔸𝔻𝔼𝕍 TORNADO 𝕌𝕊𝔼ℝ𝔹𝕆𝕋"
 PM_IMG = "https://telegra.ph/file/6f739e51ecfc6bf7ee9de.jpg"
 pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
 pm_caption += "➥ **SYSTEMS STATS**\n"
@@ -13,9 +13,9 @@ pm_caption += "➥ **Current Branch** : `master`\n"
 pm_caption += f"➥ **Version** : `2.0`\n"
 pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
 pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/SRIDHAR2021SIDDHARTH/MAHADEV-TANDAV-USERBOT/blob/master/LICENSE)\n"
-pm_caption += "➥ **Copyright** : By [𝕄𝔸ℍ𝔸𝔻𝔼𝕍 𝕌𝕊𝔼ℝ𝔹𝕆𝕋](https://t.me/TANDAV_USERBOT_SUPPORT)\n"
-pm_caption += "[Assistant By 𝕄𝔸ℍ𝔸𝔻𝔼𝕍 𝕌𝕊𝔼ℝ𝔹𝕆𝕋](https://t.me/tandav_userbot_Support)"
+pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/SRIDHAR2021SIDDHARTH/MAHADEV-TANDAV-USERBOTS/blob/master/LICENSE)\n"
+pm_caption += "➥ **Copyright** : By [𝕄𝔸ℍ𝔸𝔻𝔼𝕍 TORNADO 𝕌𝕊𝔼ℝ𝔹𝕆𝕋](https://t.me/MAHADEV_TORNADO_USERBOT_OFFICIAL)\n"
+pm_caption += "[Assistant By 𝕄𝔸ℍ𝔸𝔻𝔼𝕍 TORNADO 𝕌𝕊𝔼ℝ𝔹𝕆𝕋](https://t.me/MAHADEV_TORNADO_USERBOT_SUPPORT)"
 
 # only Owner Can Use it
 @tgbot.on(events.NewMessage(pattern="^/alive", func=lambda e: e.sender_id == bot.uid))
